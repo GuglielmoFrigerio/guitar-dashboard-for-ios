a4c2779b27140fd54c8794ed37f84e5138bee3dd
Guglielmo Frigerio <guglielmo.frigerio@gmail.com>
2021-12-22 12:44:51 +0100

    First code commit

---
+++ guitar-dashboard-for-ios/guitar-dashboard-for-ios/guitar_dashboard_for_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  guitar_dashboard_for_iosApp.swift
+//  guitar-dashboard-for-ios
+//
+//  Created by Guglielmo Frigerio on 22/12/21.
+//
+
+import SwiftUI
+
+@main
+struct guitar_dashboard_for_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ guitar-dashboard-for-ios/guitar-dashboard-for-iosUITests/guitar_dashboard_for_iosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  guitar_dashboard_for_iosUITestsLaunchTests.swift
+//  guitar-dashboard-for-iosUITests
+//
+//  Created by Guglielmo Frigerio on 22/12/21.
+//
+
+import XCTest
+
+class guitar_dashboard_for_iosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
